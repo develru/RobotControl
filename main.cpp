0b8c9ee2114c2189f8d967f13af209214dcf8757
Richard Ruzsa <develru@Richards-MacBook-Air.local>
2014-04-01 19:02:32 +0200

    Register the connection class to qml.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include <QtQml>
+
 #include "qtquick2controlsapplicationviewer.h"
+#include "src/prconnection.h"
 
 int main(int argc, char *argv[])
 {
     Application app(argc, argv);
 
     QtQuick2ControlsApplicationViewer viewer;
+
+    // Register the class
+    qmlRegisterType<PRConnection>("com.develru.prc", 1, 0, "PRCConnection");
+
     viewer.setMainQmlFile(QStringLiteral("qml/RobotControl/main.qml"));
     viewer.show();
 
