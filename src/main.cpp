158209148cb055cd5cd435f766eaed96bff14d73
rschwalk <richard.schwalk@gmail.com>
2018-03-31 18:51:36 +0200

    The minimal qt project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <QtWidgets/QApplication>
+#include <QtQml/QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
